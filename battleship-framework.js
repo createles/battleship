c40b666404e906da760196b080043e4953e704c4
createlsworklappy <justinecastillo7@gmail.com>
2025-09-02 15:44:20 +0900

    func: implement proper 10x10 grid construction
    add placeShip function to handle populating gameboard
    
    implement receiveAttack to record attack results

--- battleship-framework.js
+++ battleship-framework.js
@@ -17,5 +17,89 @@ class Ship {
 }
 
 class Gameboard {
-  constructor()
+  constructor() {
+    this.shipGrid = createGrid(); // grid for placing ships
+    this.attackGrid = createGrid(); // grid for record keeping of moves
+  }
+
+  placeShip(length, startPos, orientation) {
+    if (placeValidation(length, startPos, orientation)) { // validate placement
+      const newShip = new Ship(length); // create ship object with length
+      if (orientation === "vertical") { // populate vertically
+        for (let i = 0; i < length; i++) {
+          this.shipGrid[startPos.x + i][startPos.y] = newShip;
+        }
+      } else if (orientation === "horizontal") { // populate horizontally
+        for (let i = 0; i < length; i++) {
+          this.shipGrid[startPos.x][startPos.y + i] = newShip;
+        }
+      }
+    }
+  }
+
+  receiveAttack(pos) { // records and updates attack result
+    const target = this.shipGrid[pos.x][pos.y];
+    if (target !== null) { // if position is occupied
+      target.hit(); // update ship health
+      this.attackGrid[pos.x][pos.y] = "hit!"; // record hit on position
+      return true;
+    } else if (target === null) { // if empty position
+      this.attackGrid[pos.x][pos.y] = "miss!"; // record miss on position
+      return false
+    }
+  }
 }
+
+function createGrid() {
+  const grid = [];
+  for (let i = 0; i < 10; i++) {
+    grid[i] = [];
+    for (let j = 0; j < 10; j++) {
+      grid[i].push(null);
+    }
+  }
+  return grid;
+}
+
+function placeValidation(length, startPos, orientation) {
+  // fail-first approach (helpful for validation)
+  const xPos = startPos.x;
+  const yPos = startPos.y;
+
+  if (typeof length !== "number" || length < 2 || length > 5) return false; // if ship length is not valid, exit
+
+  if (
+    // if startPos is off-board, exit
+    xPos < 0 ||
+    xPos > 9 ||
+    yPos < 0 ||
+    yPos > 9 ||
+    this.shipGrid[xPos][yPos] !== null
+  )
+    return false;
+
+  if (orientation !== "vertical" && orientation !== "horizontal") return false;
+
+  // if move exceeds board due to ship length, exit
+  if (orientation === "vertical" && xPos + length > 10) {
+    return false;
+  } else if (orientation === "horizontal" && yPos + length > 10) {
+    return false;
+  }
+
+  // check if consecutive spaces are available for placement
+  for (let i = 0; i < length; i++) {
+    if (orientation === "vertical") {
+      if (this.shipGrid[xPos + i][yPos] !== null) {
+        return false;
+      }
+    } else if (orientation === "horizontal") {
+      if (this.shipGrid[xPos][yPos + i] !== null) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+export { Gameboard };
