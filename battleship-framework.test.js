b27b8381060f930364aa8b622ee0fd35187f0216
createlsworklappy <justinecastillo7@gmail.com>
2025-09-02 15:44:36 +0900

    test: test gameboard generation

---
+++ battleship-framework.test.js
@@ -0,0 +1,29 @@
+import { Gameboard } from "./battleship-framework";
+
+describe("test gameboard generation", () => {
+  test("creates 2 gameboards with 10 tiles", () => {
+    const gameboardA = new Gameboard();
+    expect(gameboardA.shipGrid).toEqual([[null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,]]
+    );
+    expect(gameboardA.attackGrid).toEqual([[null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,],
+      [null,null,null,null,null,null,null,null,null,null,]]
+    );
+  })
+});
