9a5b2504eec115ffdadc3303ef85dca7a1e1ddbe
createlsworklappy <justinecastillo7@gmail.com>
2025-09-03 15:13:17 +0900

    func: initialize page-element handler file
    test creating base grid dom structures w/ identifying styles

---
+++ page-elements.js
@@ -0,0 +1,21 @@
+function createTiles(playerBoard) {
+  const board = playerBoard;
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement("div");
+    row.id = `row-${i}`;
+    row.classList.add("row-container");
+    for (let j = 0; j < 10; j++) {
+      const tile = document.createElement("div");
+      tile.id = `${i}-tile-${j}`;
+      tile.classList.add("tile");
+      row.append(tile);
+    }
+    board.append(row);
+  }
+
+}
+
+const playerBoard = document.querySelector("#playerBoard");
+const opponentBoard = document.querySelector("#opponentBoard")
+createTiles(playerBoard);
+createTiles(opponentBoard);
